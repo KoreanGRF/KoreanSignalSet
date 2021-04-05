eab1ad99f828f458a5cfd92ec5f8ec4962898fbd
TELK <telk5093@gmail.com>
2021-04-05 18:13:21 +0900

    Add: Implement rest signals

--- src/electric/__init__.py
+++ src/electric/__init__.py
@@ -2,25 +2,43 @@
   한국 신호기 세트(Korean Signal Set)
   * Github repository      : https://github.com/KoreanGRF/KoreanSignalSet
 """
-
-print("Electric")
+from template import *
 
 output = ""
 
-import block
-output = output + block.output
+# Electric, block
+print("Electric, block")
+output = output + replace_electric_signal('block', 1275)
+output = output + replace_electric_signal('block', 6262)   # Restricted signal (with blue pole)
 
-import entrance
-output = output + entrance.output
+# Electric, entrance
+print("Electric, entrance")
+output = output + replace_electric_signal('entrance', 5082)
+output = output + replace_electric_signal('entrance', 6278)   # Restricted signal (with blue pole)
 
-import exit
-output = output + exit.output
+# Electric, exit
+print("Electric, exit")
+output = output + replace_electric_signal('exit', 5098)
+output = output + replace_electric_signal('exit', 6294)   # Restricted signal (with blue pole)
 
-import combo
-output = output + combo.output
+# Electric, combo
+print("Electric, combo")
+output = output + replace_electric_signal('combo', 5114)
+output = output + replace_electric_signal('combo', 6310)   # Restricted signal (with blue pole)
 
-import oneway_path
-output = output + oneway_path.output
+# Electric, path
+print("Electric, path")
+output = output + replace_electric_signal('path', 5210)
+output = output + replace_electric_signal('path', 5226)   # Seems like dummy, (with purple pole)
+output = output + replace_electric_signal('path', 6389)   # Restricted signal (with blue pole)
 
-import path
-output = output + path.output
+# Electric, oneway path
+print("Electric, oneway path")
+output = output + replace_electric_signal('oneway_path', 5194)
+output = output + replace_electric_signal('oneway_path', 5242)   # Seems like dummy (with purple pole)
+output = output + replace_electric_signal('oneway_path', 6405)   # Restricted signal (with blue pole)
+
+# Electric, programmable
+print("Electric, programmable")
+output = output + replace_electric_signal('programmable', 6175)
+output = output + replace_electric_signal('programmable', 6207)   # Restricted signal (with blue pole)
--- src/template.py
+++ src/template.py
@@ -3,8 +3,8 @@
   * Github repository      : https://github.com/KoreanGRF/KoreanSignalSet
 """
 
-# Template
-template = """
+# Template for electric
+template_electric = """
 // red
 if(is_realistic) {
     replace({num1}, "./src/{gfx_code}/R.real.png") {
@@ -18,56 +18,92 @@ if(is_realistic) {
 
 // green
 if(use_caution_signal) {
-	if(is_realistic) {
-		if(use_blue) {
-			replace({num2}, "./src/{gfx_code}/YB.real.png") {
-				{template_name}()
-			}
-		} else {
-			replace({num2}, "./src/{gfx_code}/YG.real.png") {
-				{template_name}()
-			}
-		}
-	} else {
-		if(use_blue) {
-			replace({num2}, "./src/{gfx_code}/YB.nonreal.png") {
-				{template_name}()
-			}
-		} else {
-			replace({num2}, "./src/{gfx_code}/YG.nonreal.png") {
-				{template_name}()
-			}
-		}
-	}
+    if(is_realistic) {
+        if(use_blue) {
+            replace({num2}, "./src/{gfx_code}/YB.real.png") {
+                {template_name}()
+            }
+        } else {
+            replace({num2}, "./src/{gfx_code}/YG.real.png") {
+                {template_name}()
+            }
+        }
+    } else {
+        if(use_blue) {
+            replace({num2}, "./src/{gfx_code}/YB.nonreal.png") {
+                {template_name}()
+            }
+        } else {
+            replace({num2}, "./src/{gfx_code}/YG.nonreal.png") {
+                {template_name}()
+            }
+        }
+    }
 } else {
-	if(is_realistic) {
-		if(use_blue) {
-			replace({num2}, "./src/{gfx_code}/B.real.png") {
-				{template_name}()
-			}
-		} else {
-			replace({num2}, "./src/{gfx_code}/G.real.png") {
-				{template_name}()
-			}
-		}
-	} else {
-		if(use_blue) {
-			replace({num2}, "./src/{gfx_code}/B.nonreal.png") {
-				{template_name}()
-			}
-		} else {
-			replace({num2}, "./src/{gfx_code}/G.nonreal.png") {
-				{template_name}()
-			}
-		}
-	}
+    if(is_realistic) {
+        if(use_blue) {
+            replace({num2}, "./src/{gfx_code}/B.real.png") {
+                {template_name}()
+            }
+        } else {
+            replace({num2}, "./src/{gfx_code}/G.real.png") {
+                {template_name}()
+            }
+        }
+    } else {
+        if(use_blue) {
+            replace({num2}, "./src/{gfx_code}/B.nonreal.png") {
+                {template_name}()
+            }
+        } else {
+            replace({num2}, "./src/{gfx_code}/G.nonreal.png") {
+                {template_name}()
+            }
+        }
+    }
 }
 """
 
-def add_signal_direction(gfx_code, template_name, num1, num2):
-	output = str(template)
-	output = output.replace('{num1}', str(num1))
-	output = output.replace('{num2}', str(num2))
-	output = output.replace('{gfx_code}', gfx_code)
-	output = output.replace('{template_name}', template_name)
-	return output
+# Template for semaphore
+template_sempahore = """
+// red
+replace({num1}, "./src/{gfx_code}/R.png") {
+    {template_name}()
+}
+// green
+replace({num2}, "./src/{gfx_code}/G.png") {
+    {template_name}()
+}
+"""
+
+def signal_direction(tmpl, gfx_code, template_name, num1, num2):
+    output = str(tmpl)
+    output = output.replace('{num1}', str(num1))
+    output = output.replace('{num2}', str(num2))
+    output = output.replace('{gfx_code}', gfx_code)
+    output = output.replace('{template_name}', template_name)
+    return output
+
+def replace_electric_signal(gfx_code, start_num):
+    output = ""
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_NE', start_num,    start_num+1)  + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_SW', start_num+2,  start_num+3)  + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_SE', start_num+4,  start_num+5)  + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_NW', start_num+6,  start_num+7)  + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_W',  start_num+8,  start_num+9)  + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_E',  start_num+10, start_num+11) + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_N',  start_num+12, start_num+13) + "\n"
+    output = output + signal_direction(template_electric,  'electric/' + gfx_code, 'electric_signal_S',  start_num+14, start_num+15) + "\n"
+    return output
+
+def replace_semaphore_signal(gfx_code, start_num):
+    output = ""
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_NE', start_num,    start_num+1)  + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_SW', start_num+2,  start_num+3)  + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_SE', start_num+4,  start_num+5)  + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_NW', start_num+6,  start_num+7)  + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_W',  start_num+8,  start_num+9)  + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_E',  start_num+10, start_num+11) + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_N',  start_num+12, start_num+13) + "\n"
+    output = output + signal_direction(template_sempahore, 'semaphore/' + gfx_code, 'semaphore_signal_S',  start_num+14, start_num+15) + "\n"
+    return output
